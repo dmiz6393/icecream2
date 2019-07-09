3c71aa221f09b62287709b47c3bc23aabb8d323b
Scott <galejscott@gmail.com>
2019-07-09 18:58:48 +0100

    sam is a god

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,22 @@ class OrdersController < ApplicationController
 #     session[:order_id]=nil 
 #    end 
 
+  private
+
+  def order_params
+    params.require(:order).permit(
+      :id,
+      :user_id, :ice_cream_shop_id)
+
+  end
+
+   def order_params
+    params.require(:order).permit(
+      user_ids:,
+      ice_cream_ids:[],
+      ice_creams_attributes:[:flavor],
+      total_price:)
+  end
 
 
 end
