2631e07078984c75cd8e4837914cf46d7668fb90
Danielle <dmiz@brandeis.edu>
2019-07-09 17:46:13 +0100

    postgres

--- db/migrate/20190709133828_create_ice_cream_orders.rb
+++ db/migrate/20190709133828_create_ice_cream_orders.rb
@@ -2,7 +2,7 @@ class CreateIceCreamOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :ice_cream_orders do |t|
       t.integer :quantity
-      t.references :icecream, foreign_key: true
+      t.references :ice_cream, foreign_key: true
       t.references :order, foreign_key: true
 
       t.timestamps
